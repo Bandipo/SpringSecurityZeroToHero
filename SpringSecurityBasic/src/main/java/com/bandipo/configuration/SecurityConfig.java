3eddfac35adeba9a40ff6fbca3ffd8b2e0ec5890
Bandipo Taiye <bandipotaiye@gmail.com>
2021-12-16 02:33:26 +0100

    added Postgres Database config and dependency

--- SpringSecurityBasic/src/main/java/com/bandipo/configuration/SecurityConfig.java
+++ SpringSecurityBasic/src/main/java/com/bandipo/configuration/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.bandipo.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -32,16 +37,43 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password("1234")
-                .authorities(new String[]{"Read", "Write"})
-                .and()
-                .withUser("user")
-                .password("1234")
-                .authorities("read")
-                .and()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+//        auth.inMemoryAuthentication()
+//                .withUser("admin")
+//                .password("1234")
+//                .authorities(new String[]{"Read", "Write"})
+//                .and()
+//                .withUser("user")
+//                .password("1234")
+//                .authorities("read")
+//                .and()
+//                .passwordEncoder(NoOpPasswordEncoder.getInstance());
 
+
+
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+
+
+        UserDetails admin = User.withUsername("admin")
+                .password("1234")
+                .authorities("Read", "Write")
+                .build();
+
+        UserDetails user = User.withUsername("user")
+                .password("1234")
+                .authorities("Read")
+                .build();
+
+
+        userDetailsService.createUser(admin);
+        userDetailsService.createUser(user);
+
+        auth.userDetailsService(userDetailsService);
+
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
