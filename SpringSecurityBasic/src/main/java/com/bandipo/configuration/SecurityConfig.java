a9346b963ab4cea3f30adb888b5ca5c179f876be
Bandipo Taiye <bandipotaiye@gmail.com>
2021-12-16 02:50:30 +0100

    Revert "added Postgres Database config and dependency"
    This reverts commit 3eddfac3

--- SpringSecurityBasic/src/main/java/com/bandipo/configuration/SecurityConfig.java
+++ SpringSecurityBasic/src/main/java/com/bandipo/configuration/SecurityConfig.java
@@ -5,35 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-<<<<<<< HEAD
-<<<<<<< HEAD
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-=======
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
->>>>>>> parent of 47568a9 (stucked here for now)
-=======
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.sql.DataSource;
-import java.util.Arrays;
-import java.util.Collections;
->>>>>>> 90159418741e6536951fd945181079076b9d8b22
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -46,24 +22,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //        http.authorizeRequests().anyRequest().permitAll().and().formLogin().and().httpBasic();
 
 
-       http.cors().and().csrf().ignoringAntMatchers("/contact").csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
-               .authorizeRequests()
-               .antMatchers("/contact").permitAll()
-               .antMatchers("/notices").permitAll()
+       http.authorizeRequests()
                .antMatchers("/my-account").authenticated()
                .antMatchers("/my-cards").authenticated()
                .antMatchers("/my-loans").authenticated()
                .antMatchers("/my-balance").authenticated()
+               .antMatchers("/contact").permitAll()
+               .antMatchers("/notices").permitAll()
           .and()
           .formLogin().and()
           .httpBasic();
 
     }
 
-<<<<<<< HEAD
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-<<<<<<< HEAD
 //        auth.inMemoryAuthentication()
 //                .withUser("admin")
 //                .password("1234")
@@ -98,80 +71,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
-=======
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password("1234")
-                .authorities(new String[]{"Read", "Write"})
-                .and()
-                .withUser("user")
-                .password("1234")
-                .authorities("read")
-                .and()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
->>>>>>> parent of 47568a9 (stucked here for now)
-=======
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-////        auth.inMemoryAuthentication()
-////                .withUser("admin")
-////                .password("1234")
-////                .authorities(new String[]{"Read", "Write"})
-////                .and()
-////                .withUser("user")
-////                .password("1234")
-////                .authorities("read")
-////                .and()
-////                .passwordEncoder(NoOpPasswordEncoder.getInstance());
-//
-//
-//        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
-//
-//
-//        UserDetails admin = User.withUsername("admin")
-//                .password("1234")
-//                .authorities("Read", "Write")
-//                .build();
-//
-//        UserDetails user = User.withUsername("user")
-//                .password("1234")
-//                .authorities("Read")
-//                .build();
-//
-//
-//        userDetailsService.createUser(admin);
-//        userDetailsService.createUser(user);
-//
-//        auth.userDetailsService(userDetailsService);
-//
-//    }
-
-
-
-
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
-
-
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource(){
-        final CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOrigins(Collections.singletonList("*"));
-        configuration.setAllowedMethods(Collections.singletonList("*"));
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedHeaders(Collections.singletonList("*"));
-        configuration.setMaxAge(3600L);
-
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
->>>>>>> 90159418741e6536951fd945181079076b9d8b22
 
     @Bean
     public PasswordEncoder passwordEncoder(){
